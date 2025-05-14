3fedf3fc13048f434e614b329607f5e273e7cc2c
yanjinqiang <yanjinqiang@zofund.com>
2025-05-14 10:12:55 +0800

    feat: 添加sass全局注入文件

--- config/index.ts
+++ config/index.ts
@@ -58,6 +58,13 @@ export default defineConfig<"webpack5">(async (merge, { command, mode }) => {
     cache: {
       enable: false // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
     },
+    sass: {
+      resource: [
+        path.resolve(__dirname, "..", "src/assets/css/variables.scss"),
+        path.resolve(__dirname, "..", "src/assets/css/mixin.scss"),
+        path.resolve(__dirname, "..", "src/assets/css/function.scss")
+      ]
+    },
     mini: {
       postcss: {
         url:{
